4b93d6652b95273ef6c1a2429f98da87a2c06607
talhanawaz568 <alnafi789@gmail.com>
2026-03-26 13:04:49 +0000

    first commit

---
+++ Lab_10/person_info.py
@@ -0,0 +1,17 @@
+person_info = {
+	'name': 'Alice',
+	'age': 30,
+	'city': 'New York'
+}
+name_value = person_info['name']
+print("Name:", name_value)
+
+#Updating the age value
+person_info['age'] = 31
+
+# Adding a new key-value pair
+person_info['occupation'] = 'Engineer'
+
+# Print updated dictionary
+print("Updated Dictionary:", person_info)
+
